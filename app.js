eb60cbe73f18334da2744a59754d6430ec8cf49b
Adam-rk <101628484+Adam-rk@users.noreply.github.com>
2023-04-06 08:06:35 +0200

    addition of an interval time and handling of the short block element

---
+++ app.js
@@ -0,0 +1,26 @@
+function removeShortVideos() {
+  const spans = document.querySelectorAll(
+    "span.style-scope.ytd-thumbnail-overlay-time-status-renderer"
+  );
+
+  const shorts = document.querySelectorAll("ytd-reel-shelf-renderer.style-scope.ytd-item-section-renderer");
+
+  shorts.forEach((short) => {
+    short.remove()
+  })
+  
+
+  spans.forEach((span) => {
+    const time = span.innerHTML;
+
+    if (time === "SHORTS") {
+      span.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.remove();
+    } else {
+      if (time.length < 11) {
+        span.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.remove();
+      }
+    }
+  });
+}
+
+setInterval(removeShortVideos, 100); 
